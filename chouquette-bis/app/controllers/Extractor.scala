55f4d03d5d2f764fe108aa7ee4082e2089dc9ce4
Anne-Gaëlle Schall <anne.gaelle.schall@eisti.fr>
2019-03-27 11:17:47 +0100

    Modification structure REST avec play + ca compile + retourne JSON avec la route

--- chouquette-bis/app/controllers/Extractor.scala
+++ chouquette-bis/app/controllers/Extractor.scala
@@ -1,17 +1,31 @@
 package controllers
 
+// import javax.inject.Inject
+// import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+
+import models.Product
+
+// object Extractor {
+//   implicit val locationName = Extractor("")
+// }
 
 class Extractor extends Controller {
+
+  def semanticText(text: String) = ???
+
+  def retreive(text: String) = ???
  
-  def index = TODO
- 
-  def create = TODO
- 
-  def read(id: String) = TODO
- 
-  def update(id: String) = TODO
- 
-  def delete(id: String) = TODO
+  // JSON response
+  def extract(text: String) = Action {
+    val text = Product(1, "GOOD")
+    Ok(Json.toJson(text))
+  }
+  // Action.async {
+  //   semanticText(text).retreive()
+  // }
 
 }
