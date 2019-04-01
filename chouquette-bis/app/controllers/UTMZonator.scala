dae332abc30eb37f2681acab4d449e233758e3e8
Anne-Gaëlle Schall <anne.gaelle.schall@eisti.fr>
2019-04-01 16:54:16 +0200

    Avançage d'UTMZonator, POST request works

--- chouquette-bis/app/controllers/UTMZonator.scala
+++ chouquette-bis/app/controllers/UTMZonator.scala
@@ -9,7 +9,9 @@ import scala.concurrent.ExecutionContext
 import play.api._
 import play.api.mvc._
 import play.api.libs.ws._
-import play.api.libs.json._
+import play.api.libs.json._// JSON library
+import play.api.libs.json.Reads._ // Custom validation helpers
+import play.api.libs.functional.syntax._ // Combinator syntax
 import play.api.http.HttpEntity
 
 import akka.actor.ActorSystem
@@ -26,26 +28,59 @@ object UTMZone {
   implicit val extractedTextWrites = Json.writes[ExtractedText]
 }
 
+case class Coords(long: Double, lat: Double)
+object Coords {
+  implicit val coordsReads: Reads[Coords] = (
+    (JsPath \ "long").read[Double] and
+    (JsPath \ "lat").read[Double]
+  )(Coords.apply _)
+}
+
 class UTMZonator @Inject() (
   ws: WSClient // for the http request
 )(
   implicit ec: ExecutionContext // for the http response
 ) extends Controller {
 
-  def zonator(coordonnees: String) = Action { //.async {
-    Ok(coordonnees);
-    // ws
-    //   .url("http://icc.pau.eisti.fr/rest/annotate?text="+textToAnnotate)
-    //   .withHeaders("Accept" -> "application/json")
-    //   .get()
-    //   .map(validator)
-    //   .map {
-    //     case Some(extractedText) => {
-    //       println(extractedText)
-    //       Ok( extractedText.flatMap( semanticText => (semanticText \\ "@URI") ).toString )
-    //     }
-    //     case None => NotFound("Not found")
-    //   }
+  def validatorJsValue(coord: String): JsValue = {
+    return (Json.parse(coord));
+  }
+
+  def zonatora = Action(parse.json) {res => {
+    println("POST REQUEST")
+    val optionOfCoords = res.body.validate[Seq[Coords]].asOpt
+    // println(optionOfCoords.map(x => Ok(x.toString)))
+    optionOfCoords.map( x => Ok(x.toString) ).getOrElse(BadRequest("Mauvaise requete... "))
+  }}
+
+  def zonator(coordonnees: String) = Action.async {
+    println("-------------------------------------------");
+    // val coordJson = validatorJsValue(coordonnees);
+    // println(coordJson);
+    
+    // val latReads: Reads[Coords] = (JsPath \ "lat").read[Coords]
+    // val latResult = coordJson.validate[Seq[Coords]]
+    // latResult match {
+    //   case s: JsSuccess[Coords] => println("Lat: " + s.get)
+    //   case e: JsError => println("Errors: " + JsError.toJson(e).toString() +"\n"+e)
+    // }
+
+    println("-------------------------------------------");
+    println("-------------------------------------------");
+
+    ws
+      .url("https://www.latlong.net/c/?lat=35.000000&long=600.000000")
+      .withHeaders("Accept" -> "application/json")
+      .get()
+      .map((res) => {
+        // println("------------------------------------------------------------------");
+        // println("RESULTTAAAAT:");
+        // println(res);
+        // println(res.body);
+        Ok(coordonnees);
+      }) 
   }
 
 }
+
+// API Key : 4e76f5429883420b92d7e90569089f7c
