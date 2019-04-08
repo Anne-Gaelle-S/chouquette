8b7ff62da5a8dd988ffa5888bfaad23d5d5a8f67
Axel Nussbaumer <naxel@laposte.net>
2019-04-08 14:08:45 +0200

    changed Descriptor to mock api server for tests

--- chouquette/app/controllers/1-descriptor/Descriptor.scala
+++ chouquette/app/controllers/1-descriptor/Descriptor.scala
@@ -4,13 +4,14 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import scala.concurrent._
-import play.api.http.Status
+import play.api.http.{ Status => StatusCode }
 import play.api.libs.json._
 import play.api.libs.ws._ // pour faire les requetes http (le client http quoi)
 
 // Service 1 :
-// Récupérer un texte de randonnée via l’API ​ https://api.camptocamp.org/outings/​
-// ou son extraction ​ https://choucas.blqn.fr/data/outing/
+// Récupérer un texte de randonnée via l’API ​:
+//   https://api.camptocamp.org/outings/​
+// ou son extraction : https://choucas.blqn.fr/data/outing/
 
 case class Description(description: String)
 
@@ -19,21 +20,27 @@ object Description {
 }
 
 @Singleton
-class Descriptor @Inject()(
+class Descriptor(
   cc: ControllerComponents,
-  ws: WSClient
+  ws: WSClient,
+  baseUrl: String
 )(
   implicit ec: ExecutionContext
 ) extends AbstractController(cc) {
 
-  def validator(response: WSResponse): Option[Description] = {
-    if(response.status == play.api.http.Status.OK)
+  @Inject def this(
+    cc: ControllerComponents,
+    ws: WSClient,
+    ec: ExecutionContext
+  ) = this(cc, ws, "https://choucas.blqn.fr")(ec)
+
+  def validator(response: WSResponse): Option[Description] =
+    if (response.status == StatusCode.OK)
       response.json.validate[Seq[Description]].asOpt.flatMap(_.headOption)
     else None
-  }
 
   def requestMe(id: Int) = Action.async {
-    ws.url("https://choucas.blqn.fr/data/outing/"+id)
+    ws.url(s"$baseUrl/data/outing/"+id)
       .get
       .map(validator)
       .map {
